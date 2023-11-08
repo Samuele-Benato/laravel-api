ded741c62000f2134b790f2e4498d5614a9355a2
Samuele-Benato <samuele.benato@gmail.com>
2023-11-08 12:07:13 +0100

    fix : ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -32,6 +32,19 @@ class ProjectController extends Controller
         $project = Project::with('type:id,label', 'technologies:id,label')
             ->where('id', $id)
             ->first();
+
+        if (!$project)
+            abort(404, "Progetto non trovato");
+
         return response()->json($project);
+    }
+
+    public function projectByType($type_id)
+    {
+        $projects = Project::with('type:id,label', 'technologies:id,label')
+            ->orderBy('id', 'desc')
+            ->where('type_id', $type_id)
+            ->paginate(5);
+        return response()->json($projects);
     }
 }
